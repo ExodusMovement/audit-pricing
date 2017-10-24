04253b44de0903e69f845fdd3719b08661111ce2
RyanZim <opensrc@ryanzim.com>
2017-10-24 10:39:39 -0400

    Update

---
+++ diff.sh
@@ -0,0 +1,1 @@
+diff -r fixtures/$1/$2 results/$1/$2
--- test.js
+++ test.js
@@ -1,22 +1,25 @@
 import test from 'ava'
+import fs from 'fs-extra'
 import fetch from 'node-fetch'
 import { histoDay } from 'cryptocompare'
 global.fetch = fetch
 
 const HISTO_DAYS = 500
-// Skipping DOGE here
-const cryptos = ['REP', 'BTC', 'DASH', 'DCR', 'ETH', 'ETC', 'GNT', 'LTC']
+const cryptos = ['REP', 'BTC', 'BCH', 'DASH', 'DCR', 'ETH', 'ETC', 'GNT', 'LTC', 'XRP', 'OMG']
 const fiats = ['USD', 'EUR', 'GBP', 'CAD']
 
 for (const crypto of cryptos) {
   for (const fiat of fiats) {
     test(`cryptocompare: ${crypto}/${fiat}`, async t => {
+      const oldData = await fs.readFile(`fixtures/${crypto}/${fiat}`, 'utf8').catch(() => {})
       const data = await histoDay(crypto, fiat, {
         limit: HISTO_DAYS,
         // Locking in the timestamp so data shouldn't change
-        timestamp: new Date('2017-07-05')
+        timestamp: new Date('2017-10-24')
       })
-      t.snapshot(data)
+      const json = JSON.stringify(data, null, 2)
+      await fs.outputFile(`results/${crypto}/${fiat}`, json)
+      t.true(oldData === json, `${crypto}/${fiat}`)
     })
   }
 }
